7667f6a8a11293f6d13f9c294172eb0c85004cd8
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2026-02-07 22:16:37 +0530

    feat: add blog pages with Contentful integration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="border-t py-4 md:py-0 bg-gray-100">
       <div className="container mx-auto flex h-24 items-center justify-center">
         <p className="text-center text-sm leading-loose text-gray-800">
-          all right reserved @ created by Deepyaman Mondal
+          all right reserved 2026 @ created by Deepyaman Mondal
         </p>
       </div>
     </footer>
